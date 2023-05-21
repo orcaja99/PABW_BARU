e86954310b5f24f3c81aa95c9b5a8031bc3051d9
TrixTin23 <11201018@student.itk.ac.id>
2023-05-21 23:55:35 +0800

    rewind merge

--- resources/views/profileuser.blade.php
+++ resources/views/profileuser.blade.php
@@ -1,84 +1,90 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
 
-@section('content')
-<link href=/css/profileuserakunsaya.css rel='stylesheet' type='text/css'>
-<div class="row gx-5">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/css/profileuserakunsaya.css">
+    <title>Profile User</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+    <nav class="navbar navbar-dark" data-bs-theme="dark">
+        <div class="container">
+
+            <a class="navbar-brand" href="#">
+                <img src="img/TicketIn.png" alt="TicketIn" width="120" height="26">
+            </a>
+            <!-- <button type="button" class="btn btn-success">Success</button> -->
+            <button class="btn btn-light" type="submit">Keluar</button>
+
+        </div>
+    </nav>
+</head>
+
+<body>
+
+    <!-- Example Code -->
+
+
+
+    <div class="row">
         <div class="container text-center">
             <div class="row justify-content-evenly">
                 <div class="col-4">
-         <div class="p-5">
-          <div class="card" style="width: 20dp; ">
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">{{ Auth::user()->name }}</li>
-              <li class="list-group-item">TicketInPay</li>
-              <li class="list-group-item">Pesanan</li>
-              <li class="list-group-item">Riwayat Pemesanan</li>
-            </ul>
-          </div>
-         </div>
-        </div>
-        <div class="col">
-          <div class="p-5">
-            <div class="card" style="width: 20dp;">
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Data Pribadi</li>
-                <label for="exampleFormControlInput1" class="form-label">Nama Lengkap</label>
-                <li class="list-group-item"><input class="form-control" type="text" placeholder="{{ Auth::user()->name }}" aria-label="Disabled input example" disabled></li>
-                <label for="exampleFormControlInput1" class="form-label">Email</label>
-                <li class="list-group-item"><input class="form-control" type="text" placeholder="{{ Auth::user()->email }}" aria-label="Disabled input example" disabled></li>
-                <label for="exampleFormControlInput1" class="form-label">Jenis Kelamin</label>
-                <select class="form-control" id="dropdown" name="dropdown">
-                  <option value="option1">Laki-laki</option>
-                  <option value="option2">Perempuan</option>
-                </select>
-                <label for="exampleFormControlInput1" class="form-label">Tanggal Lahir</label>
-                <input type="date" name="tanggal" class="form-control" style="height:50px" placeholder="tanggal">
-                <label for="exampleFormControlInput1" class="form-label">Nomor Telepon</label>
-                <input type="number" class="form-control">
-              </ul>
+                    <div class="container text-center">
+                        <div class="row row-cols-1">
+                            <div class="col"><img src="\img\p.png" class="rounded-circle" width="150">Fauzan Maftuh Alwafi</div>
+                            <div class="col">
+                                <ion-icon name="wallet-outline" size="small"></ion-icon> TicketInPay</div>
+                            <div class="col">
+                                <ion-icon name="reader-outline" size="small"></ion-icon> Pesanan</div>
+                            <div class="col">
+                                <ion-icon name="receipt-outline" size="small"></ion-icon> Riwayat Pemesanan</div>
+                            <div class="col">
+                                <ion-icon name="log-out-outline" size="small"></ion-icon> Keluar Akun</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-7">
+                    <p class="text-start">Data Pribadi</p>
+                    <div class="mb-3">
+                        <p for="exampleFormControlInput1" class="text-start">Nama Lengkap</p>
+                        <!-- <label for="exampleFormControlInput1" class="form-label">Nama Lengkap</label> -->
+                        <input class="form-control" type="text" placeholder="Fauzan Maftuh Alwafi" aria-label="Disabled input example" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <p for="exampleFormControlInput1" class="text-start">Jenis Kelamin</p>
+                        <!-- <label for="exampleFormControlTextarea1" class="form-label">Jenis Kelamin</label> -->
+                        <input class="form-control" type="text" placeholder="Laki-laki" aria-label="Disabled input example" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <p for="exampleFormControlInput1" class="text-start">Tanggal Lahir</p>
+                        <!-- <label for="exampleFormControlInput1" class="form-label">Tanggal Lahir</label> -->
+                        <div class="input-group">
+                            <input type="text" aria-label="date" class="form-control" placeholder="08" disabled>
+                            <input type="text" aria-label="month" class="form-control" placeholder="Juli" disabled>
+                            <input type="text" aria-label="year" class="form-control" placeholder="2002" disabled>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <p for="exampleFormControlInput1" class="text-start">Email</p>
+                        <!-- <label for="exampleFormControlTextarea1" class="form-label">Email</label> -->
+                        <input class="form-control" type="text" placeholder="fauzanmaftuh74@gmail.com" aria-label="Disabled input example" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <p for="exampleFormControlInput1" class="text-start">Nomor Telepon</p>
+                        <!-- <label for="exampleFormControlTextarea1" class="form-label">Nomor Telepon</label> -->
+                        <input class="form-control" type="text" placeholder="0895705054048" aria-label="Disabled input example" disabled>
+                    </div>
+                </div>
             </div>
-            </div>
-            </div>
         </div>
-<!-- <div class="container overflow-hidden text-center">
-      
-      <div class="row gx-5">
-        <div class="col">
-         <div class="p-5">
-          <div class="card" style="width: 20dp;">
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">{{ Auth::user()->name }}</li>
-              <li class="list-group-item">TicketInPay</li>
-              <li class="list-group-item">Pesanan</li>
-              <li class="list-group-item">Riwayat Pemesanan</li>
-            </ul>
-          </div>
-         </div>
-        </div>
-        <div class="col">
-          <div class="p-5">
-            <div class="card" style="width: 20dp;">
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Data Pribadi</li>
-                <label for="exampleFormControlInput1" class="form-label">Nama Lengkap</label>
-                <li class="list-group-item"><input class="form-control" type="text" placeholder="{{ Auth::user()->name }}" aria-label="Disabled input example" disabled></li>
-                <label for="exampleFormControlInput1" class="form-label">Email</label>
-                <li class="list-group-item"><input class="form-control" type="text" placeholder="{{ Auth::user()->email }}" aria-label="Disabled input example" disabled></li>
-                <label for="exampleFormControlInput1" class="form-label">Jenis Kelamin</label>
-                <select class="form-control" id="dropdown" name="dropdown">
-                  <option value="option1">Laki-laki</option>
-                  <option value="option2">Perempuan</option>
-                </select>
-                <label for="exampleFormControlInput1" class="form-label">Tanggal Lahir</label>
-                <input type="date" name="tanggal" class="form-control" style="height:50px" placeholder="tanggal">
-                <label for="exampleFormControlInput1" class="form-label">Nomor Telepon</label>
-                <input type="number" class="form-control">
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> -->
 
         <!-- End Example Code -->
 </body>
@@ -89,4 +95,5 @@
         </a>
     </div>
 </footer>
-@endsection
+
+</html>
