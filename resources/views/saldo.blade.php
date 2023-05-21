67e5007e0d8a634c151bd756f599be72c655d452
TrixTin23 <11201018@student.itk.ac.id>
2023-05-22 00:07:24 +0800

    add profileuser and saldo

--- resources/views/saldo.blade.php
+++ resources/views/saldo.blade.php
@@ -1,75 +1,84 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="/css/profileuserakunsaya.css">
-    <title>My Order</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-
-    <nav class="navbar navbar-dark" data-bs-theme="dark">
-        <div class="container">
-
-            <a class="navbar-brand" href="#">
-                <img src="img/TicketIn.png" alt="TicketIn" width="120" height="26">
-            </a>
-            <!-- <button type="button" class="btn btn-success">Success</button> -->
-            <button class="btn btn-light" type="submit">Keluar</button>
-
-        </div>
-    </nav>
-</head>
-
-<body>
-
-    <!-- Example Code -->
-
-
-
-    <div class="row">
+@section('content')
+<link href=/css/profileuserakunsaya.css rel='stylesheet' type='text/css'>
+<div class="row gx-5">
         <div class="container text-center">
             <div class="row justify-content-evenly">
                 <div class="col-4">
-                    <div class="container text-center">
-                        <div class="row row-cols-1">
-                            <div class="col"><img src="\img\p.png" class="rounded-circle" width="150">Fauzan Maftuh Alwafi</div>
-                            <div class="col">
-                                <ion-icon name="wallet-outline" size="small"></ion-icon> TicketInPay</div>
-                            <div class="col">
-                                <ion-icon name="reader-outline" size="small"></ion-icon> Pesanan</div>
-                            <div class="col">
-                                <ion-icon name="receipt-outline" size="small"></ion-icon> Riwayat Pemesanan</div>
-                            <div class="col">
-                                <ion-icon name="log-out-outline" size="small"></ion-icon> Keluar Akun</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-7">
-                    <div class="card">
-                        <div class="card-body">
-                        <b class="card-title">Informasi Saldo</b>
-                        <div class="card">
-                        <div class="card-body">
-                        <b class="card-title">Saldo Anda</b>
-                        <p class="card-text">Rp XXX.XXX</p>
-                        </div>
-                    </div>
-                        </div>
-                        
-                    </div>
-                    
-                    
-                    
-                </div>
+         <div class="p-5">
+          <div class="card" style="width: 20dp; ">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">{{ Auth::user()->name }}</li>
+              <li class="list-group-item">TicketInPay</li>
+              <li class="list-group-item">Pesanan</li>
+              <li class="list-group-item">Riwayat Pemesanan</li>
+            </ul>
+          </div>
+         </div>
+        </div>
+        <div class="col">
+          <div class="p-5">
+            <div class="card" style="width: 20dp;">
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">TicketinPay</li>
+                <label for="exampleFormControlInput1" class="form-label">Saldo</label>
+                <li class="list-group-item"><input class="form-control" type="text" placeholder="{{ Auth::user()->name }}" aria-label="Disabled input example" disabled></li>
+                <!-- <label for="exampleFormControlInput1" class="form-label">Email</label>
+                <li class="list-group-item"><input class="form-control" type="text" placeholder="{{ Auth::user()->email }}" aria-label="Disabled input example" disabled></li>
+                <label for="exampleFormControlInput1" class="form-label">Jenis Kelamin</label>
+                <select class="form-control" id="dropdown" name="dropdown">
+                  <option value="option1">Laki-laki</option>
+                  <option value="option2">Perempuan</option>
+                </select>
+                <label for="exampleFormControlInput1" class="form-label">Tanggal Lahir</label>
+                <input type="date" name="tanggal" class="form-control" style="height:50px" placeholder="tanggal">
+                <label for="exampleFormControlInput1" class="form-label">Nomor Telepon</label>
+                <input type="number" class="form-control"> -->
+              </ul>
+            </div>
+            </div>
             </div>
         </div>
+<!-- <div class="container overflow-hidden text-center">
+      
+      <div class="row gx-5">
+        <div class="col">
+         <div class="p-5">
+          <div class="card" style="width: 20dp;">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">{{ Auth::user()->name }}</li>
+              <li class="list-group-item">TicketInPay</li>
+              <li class="list-group-item">Pesanan</li>
+              <li class="list-group-item">Riwayat Pemesanan</li>
+            </ul>
+          </div>
+         </div>
+        </div>
+        <div class="col">
+          <div class="p-5">
+            <div class="card" style="width: 20dp;">
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Data Pribadi</li>
+                <label for="exampleFormControlInput1" class="form-label">Nama Lengkap</label>
+                <li class="list-group-item"><input class="form-control" type="text" placeholder="{{ Auth::user()->name }}" aria-label="Disabled input example" disabled></li>
+                <label for="exampleFormControlInput1" class="form-label">Email</label>
+                <li class="list-group-item"><input class="form-control" type="text" placeholder="{{ Auth::user()->email }}" aria-label="Disabled input example" disabled></li>
+                <label for="exampleFormControlInput1" class="form-label">Jenis Kelamin</label>
+                <select class="form-control" id="dropdown" name="dropdown">
+                  <option value="option1">Laki-laki</option>
+                  <option value="option2">Perempuan</option>
+                </select>
+                <label for="exampleFormControlInput1" class="form-label">Tanggal Lahir</label>
+                <input type="date" name="tanggal" class="form-control" style="height:50px" placeholder="tanggal">
+                <label for="exampleFormControlInput1" class="form-label">Nomor Telepon</label>
+                <input type="number" class="form-control">
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> -->
 
         <!-- End Example Code -->
 </body>
@@ -80,5 +89,4 @@
         </a>
     </div>
 </footer>
-
-</html>
+@endsection
