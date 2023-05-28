33363d107b61c8fadd717e1884d0a568dbf351b2
herliananur <herliananur19@gmail.com>
2023-05-28 21:17:03 +0800

    myorder, pesan_kamar, profileuser, saldo, ticket, profileuserakunsaya

--- resources/views/saldo.blade.php
+++ resources/views/saldo.blade.php
@@ -2,92 +2,84 @@
 
 @section('content')
 <link href=/css/profileuserakunsaya.css rel='stylesheet' type='text/css'>
-<div class="row gx-5">
-        <div class="container text-center">
-            <div class="row justify-content-evenly">
-                <div class="col-4">
-         <div class="p-5">
-          <div class="card" style="width: 20dp; ">
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">{{ Auth::user()->name }}</li>
-              <li class="list-group-item">TicketInPay</li>
-              <li class="list-group-item">Pesanan</li>
-              <li class="list-group-item">Riwayat Pemesanan</li>
-            </ul>
+
+<div class="container">
+
+      <!-- Breadcrumb -->
+      <!-- <nav aria-label="breadcrumb" class="main-breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item"><a href="javascript:void(0)">User</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Profile Settings</li>
+        </ol>
+      </nav> -->
+      <!-- /Breadcrumb -->
+
+      <div class="row gutters-sm">
+        <div class="col-md-4 d-none d-md-block">
+          <div class="card">
+            <div class="card-body">
+              <nav class="nav flex-column nav-pills nav-gap-y-1">
+                <a href="#profile" data-toggle="tab" class="nav-item nav-link has-icon nav-link-faded active">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user mr-2">
+                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>{{ Auth::user()->name }}
+                </a>
+                <a href="#TicketInPay" data-toggle="tab" class="nav-item nav-link has-icon nav-link-faded">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-credit-card mr-2"><rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect><line x1="1" y1="10" x2="23" y2="10"></line></svg>TicketInPay
+                </a>
+                <a href="#pesanan" data-toggle="tab" class="nav-item nav-link has-icon nav-link-faded">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shield mr-2">
+                    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path></svg>Pesanan
+                </a>
+                <a href="#riwayatpemesanan" data-toggle="tab" class="nav-item nav-link has-icon nav-link-faded">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell mr-2"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+                  <path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>Riwayat Pemesanan
+                </a>
+                
+              </nav>
+            </div>
           </div>
-         </div>
         </div>
-        <div class="col">
-          <div class="p-5">
-            <div class="card" style="width: 20dp;">
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">TicketinPay</li>
-                <label for="exampleFormControlInput1" class="form-label">Saldo</label>
+        <div class="col-md-8">
+          <div class="card">
+            <div class="card-header border-bottom mb-3 d-flex d-md-none">
+              <ul class="nav nav-tabs card-header-tabs nav-gap-x-1" role="tablist">
+                <li class="nav-item">
+                  <a href="#profile" data-toggle="tab" class="nav-link has-icon active"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></a>
+                </li>
+                <li class="nav-item">
+                  <a href="#account" data-toggle="tab" class="nav-link has-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg></a>
+                </li>
+                <li class="nav-item">
+                  <a href="#security" data-toggle="tab" class="nav-link has-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shield"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path></svg></a>
+                </li>
+                <li class="nav-item">
+                  <a href="#notification" data-toggle="tab" class="nav-link has-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg></a>
+                </li>
+                <li class="nav-item">
+                  <a href="#billing" data-toggle="tab" class="nav-link has-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-credit-card"><rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect><line x1="1" y1="10" x2="23" y2="10"></line></svg></a>
+                </li>
+              </ul>
+            </div>
+            <div class="card-body tab-content">
+              <div class="tab-pane active" id="profile">
+                <h6>TicketInPay</h6>
+                <hr>
+                <form>
+                  <label for="exampleFormControlInput1" class="form-label">Saldo</label>
                 <div class="input-group p-5">
                   <span class="input-group-text">Rp</span>
-                  <input type="text" class="form-control" aria-label="Amount (to the nearest rupiah)">
+                  <input type="text" class="form-control" aria-label="Amount (to the nearest rupiah)" type="text" placeholder="{{ Auth::user()->email }}" disabled>
                   <span class="input-group-text">.00</span>
                 </div>
-
-                <!-- <li class="list-group-item"><input class="form-control" type="text" placeholder="{{ Auth::user()->name }}" aria-label="Disabled input example" disabled></li> -->
-                <!-- <label for="exampleFormControlInput1" class="form-label">Email</label>
-                <li class="list-group-item"><input class="form-control" type="text" placeholder="{{ Auth::user()->email }}" aria-label="Disabled input example" disabled></li>
-                <label for="exampleFormControlInput1" class="form-label">Jenis Kelamin</label>
-                <select class="form-control" id="dropdown" name="dropdown">
-                  <option value="option1">Laki-laki</option>
-                  <option value="option2">Perempuan</option>
-                </select>
-                <label for="exampleFormControlInput1" class="form-label">Tanggal Lahir</label>
-                <input type="date" name="tanggal" class="form-control" style="height:50px" placeholder="tanggal">
-                <label for="exampleFormControlInput1" class="form-label">Nomor Telepon</label>
-                <input type="number" class="form-control"> -->
-              </ul>
-            </div>
-            </div>
-            </div>
-        </div>
-<!-- <div class="container overflow-hidden text-center">
-      
-      <div class="row gx-5">
-        <div class="col">
-         <div class="p-5">
-          <div class="card" style="width: 20dp;">
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">{{ Auth::user()->name }}</li>
-              <li class="list-group-item">TicketInPay</li>
-              <li class="list-group-item">Pesanan</li>
-              <li class="list-group-item">Riwayat Pemesanan</li>
-            </ul>
-          </div>
-         </div>
-        </div>
-        <div class="col">
-          <div class="p-5">
-            <div class="card" style="width: 20dp;">
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Data Pribadi</li>
-                <label for="exampleFormControlInput1" class="form-label">Nama Lengkap</label>
-                <li class="list-group-item"><input class="form-control" type="text" placeholder="{{ Auth::user()->name }}" aria-label="Disabled input example" disabled></li>
-                <label for="exampleFormControlInput1" class="form-label">Email</label>
-                <li class="list-group-item"><input class="form-control" type="text" placeholder="{{ Auth::user()->email }}" aria-label="Disabled input example" disabled></li>
-                <label for="exampleFormControlInput1" class="form-label">Jenis Kelamin</label>
-                <select class="form-control" id="dropdown" name="dropdown">
-                  <option value="option1">Laki-laki</option>
-                  <option value="option2">Perempuan</option>
-                </select>
-                <label for="exampleFormControlInput1" class="form-label">Tanggal Lahir</label>
-                <input type="date" name="tanggal" class="form-control" style="height:50px" placeholder="tanggal">
-                <label for="exampleFormControlInput1" class="form-label">Nomor Telepon</label>
-                <input type="number" class="form-control">
-              </ul>
+              
             </div>
           </div>
         </div>
       </div>
-    </div> -->
 
-        <!-- End Example Code -->
-</body>
+    </div>
+
 <footer class="text-white py-2">
     <div class="container mt-1 d-flex justify-content-center">
         <a class="navbar-brand" href="#">
